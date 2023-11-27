922fb7b3ac1fa5abdaa998b843c85acde041cedc
Neal Beeken <neal.beeken@mongodb.com>
2023-11-27 09:20:19 -0500

    fix: a big bug

--- index.mjs
+++ index.mjs
@@ -2,6 +2,7 @@ import myCoolFunctionHelper from './index.mjs'
 
 const myCoolFunction = (i = 1) => {
     if (i === 0) return console.log('\u{d83d}\u{de0e}\u{203c}\u{fe0f}');
+    console.log(i);
     return myCoolFunctionHelper(i - 1)
 }
 
