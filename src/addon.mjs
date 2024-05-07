748949d266943ff27616ef29440cd9f9c8113e3b
Neal Beeken <neal.beeken@mongodb.com>
2024-05-07 10:32:29 -0400

    test: release

--- src/addon.mjs
+++ src/addon.mjs
@@ -2,6 +2,31 @@ import path from "node:path";
 import process from "node:process";
 import os from "node:os";
 
-const module = { exports: {} };
-process.dlopen(module, path.resolve(import.meta.dirname, '..', 'build/Release/addon.node'), os.constants.dlopen.RTLD_NOW);
-export default module.exports;
+function tryDynamicLoad(addonPath) {
+    const module = { exports: {} };
+    try {
+        process.dlopen(module, path.resolve(import.meta.dirname, '..', addonPath), os.constants.dlopen.RTLD_NOW);
+        return module.exports;
+    } catch {
+        return null;
+    }
+}
+
+const { platform, arch } = process;
+
+const paths = [
+    'build/Debug/addon.node',
+    'build/Release/addon.node',
+    ...(platform === 'linux' ?
+      [`build/Release/addon-linux-${arch}-gnu.node`, `build/Release/addon-linux-${arch}-gnu.node`] :
+      [`build/Release/addon-${platform}-${arch}.node`]),
+]
+
+let module;
+for (const path of paths) {
+    module = tryDynamicLoad(path);
+    if (module.done) break;
+}
+
+const exports = module;
+export default exports;
